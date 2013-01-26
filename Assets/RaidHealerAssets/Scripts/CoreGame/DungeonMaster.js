3ac3989f64f8b8da29fb549d96cc8e4ec4f72644
Adam Proctor <adam@uken.com>
2013-01-26 16:11:24 -0500

    sync

--- Assets/RaidHealerAssets/Scripts/CoreGame/DungeonMaster.js
+++ Assets/RaidHealerAssets/Scripts/CoreGame/DungeonMaster.js
@@ -6,10 +6,19 @@
  * - roll a skill check to see if this code comes out clean, 1d20
  */
  
+var adventurers;
 
 
 function Start () {
+	adventurers = [];
+}
 
+function Awake () {
+	var spawnPoints = GameObject.FindGameObjectsWithTag("adv_spawn");
+	
+	for(var spawnPoint : GameObject in spawnPoints) {
+		Debug.Log(spawnPoint);
+	}
 }
 
 function Update () {
